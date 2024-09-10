1ed244a04cea2eb3cb587ef585cd8e7bde8c7000
Bob Belford <rebelford@ualr.edu>
2024-09-10 10:37:36 -0500

    Added docstring to open_pdb fct in functions.py

--- molecool/functions.py
+++ molecool/functions.py
@@ -83,6 +83,24 @@ def calculate_distance(rA, rB):
 
     # This function reads in a pdb file and returns the atom names and coordinates. 
 def open_pdb(file_location):
+     """Open and read coordinates and atom symbols from a pdb file.
+
+     The pdb file must specify the atom elements in the last column, and follow
+     the conventions outlined in the PDB format specification.
+
+     Parameters
+     ----------
+     file_location : str
+         The location of the pdb file to read in.
+
+     Returns
+     -------
+     coords : np.ndarray
+         The coordinates of the pdb file.
+     symbols : list
+         The atomic symbols of the pdb file.
+
+     """
     
     with open(file_location) as f:
         data = f.readlines()
